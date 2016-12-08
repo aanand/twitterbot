6988e7aa51abc88ba05786bdca20079400034bf9
Aanand Prasad <aanand.prasad@gmail.com>
2016-12-08 13:04:35 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='twitterbot',
     version='0.1.0',
     author='thricedotted',
     author_email='thricedotted@gmail.com',
-    packages=['twitterbot'],
+    packages=find_packages(),
     description='A simple Python framework for creating Twitter bots.',
     install_requires=[
         "tweepy >= 2.3"
